9a6dc246a8f59f39a26446c0a95a90bbb786c385
MonkeySean <lihai1988113@126.com>
2025-01-15 15:59:33 +0800

    add service

---
+++ JavaDemo/SimpleService.java
@@ -0,0 +1,9 @@
+public class SimpleService {
+    public String getMessage() {
+        return "This is a simple service message.";
+    }
+
+    public void printMessage() {
+        System.out.println("it's a service!");
+    }
+}
